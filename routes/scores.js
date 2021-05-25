86486614c35a927f983a10fd72f6f8d154be1e83
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-05-25 08:24:40 -0400

    tweaks route

--- routes/scores.js
+++ routes/scores.js
@@ -29,25 +29,46 @@ router.post("/", async (req, res) => {
   }
 });
 
+function leaderboard(scores) {
+  scores = scores.sort((a, b) => b.score - a.score);
+  let leaderboard = [{ rank: 1, name: scores[0].name, score: scores[0].score }];
+  const rankIndices = [1, 2, 3, 5, 8, 13, 21];
+  for (let i = 0; i < rankIndices.length; i++) {
+    if (rankIndices[i] < scores.length)
+      leaderboard.push({
+        rank: rankIndices[i],
+        name: scores[rankIndices[i] - 1].name,
+        score: scores[rankIndices[i] - 1].score,
+      });
+  }
+  return leaderboard;
+}
+
 router.get("/leaderboard", async (req, res) => {
   try {
     let scores = await Score.find();
-    scores = scores.sort((a, b) => b.score - a.score);
-    let leaderboard = [
-      { rank: 1, name: scores[0].name, score: scores[0].score },
-    ];
-    const rankIndices = [1, 2, 3, 5, 8, 13, 21];
-    for (let i = 0; i < rankIndices.length; i++) {
-      if (rankIndices[i] < scores.length)
-        leaderboard.push({
-          rank: rankIndices[i],
-          name: scores[rankIndices[i] - 1].name,
-          score: scores[rankIndices[i] - 1].score,
-        });
-    }
-    console.log("hello out there", scores);
-    console.log("leaders", leaderboard);
-    res.json(leaderboard);
+    const leaders = leaderboard(scores);
+    res.json(leaders);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.get("/scoreToBeat", async (req, res) => {
+  try {
+    let scores = await Score.find();
+
+    let stb;
+    let i = 20;
+    do {
+      if (scores[i]) {
+        stb = scores[i];
+      } else {
+        i--;
+      }
+    } while (!scores[i]);
+
+    res.json(stb);
   } catch (error) {
     res.json({ message: error });
   }
